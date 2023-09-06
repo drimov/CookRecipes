1b3d63ae02c783a9bec396b2b0d50372ad2191ca
Dimitri Lhopital <dimitrilho@gmail.com>
2023-09-06 21:54:32 +0400

    feat: ✨configuration additionelle pour les fonctions

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -7,6 +7,8 @@ const config = {
   printWidth: 80,
   endOfLine: "lf",
   plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+  tailwindFunctions: ["clsx"],
 }
 
 module.exports = config
