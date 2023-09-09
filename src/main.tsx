3d9278d70a3cf6825482c38176c1654a555920ca
Dimitri Lhopital <dimitrilho@gmail.com>
2023-09-09 17:30:55 +0400

    refactor: 🎨Modification des composant avec le nouveau wrapper/ suppression du code redondant

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
 import App from "./App.tsx"
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000, // 1mn
-    },
-  },
-})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 )
