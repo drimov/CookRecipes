85229287751cfd267d0157ee330dfed02fb77dcb
Dimitri Lhopital <dimitrilho@gmail.com>
2023-09-10 13:24:32 +0400

    feat: ✨ ajout de la navbar dans le RootLayout de l'application

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
+
+import Navbar from "@/components/navbar"
 
 const RootLayout = () => {
+  const { pathname } = useLocation()
+  const pathForbidden = ["/login", "/signup"]
+
   return (
-    <div className="container h-screen bg-inherit px-8 md:px-16 lg:px-24">
+    <div className="container h-screen bg-inherit px-4 md:px-8 lg:px-16">
+      {!pathForbidden.includes(pathname) ? <Navbar /> : null}
       <Outlet />
     </div>
   )
