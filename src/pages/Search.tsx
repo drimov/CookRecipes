c2d67b410122a5cc14af956d0a3cc613ecea4abc
DayOneAI <bambathiam88@gmail.com>
2023-09-12 21:56:18 +0200

    Page de recherche_V2

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,21 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import React, { useState } from "react"
 
 type SearchPageProps = {
@@ -13,16 +31,47 @@ const SearchPage: React.FC<SearchPageProps> = ({ onSearch }) => {
   }
 
   return (
-    <div>
-      <h1>Mise en place search page</h1>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search here ..."
-      />
-      <button onClick={handleSearch}>Search</button>
-    </div>
+    <Card className="mx-auto my-10 w-[350px]">
+      <CardHeader>
+        <CardTitle className="text-center">
+          What are your favorite cuisines?
+        </CardTitle>
+        <CardDescription>PERSONALIZE YOUR EXPERIENCE.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Search here ..."
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Categories</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="beef">Beef</SelectItem>
+                  <SelectItem value="chick">Chicken</SelectItem>
+                  <SelectItem value="veget">Vegetarian</SelectItem>
+                  <SelectItem value="dessert">Dessert</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button onClick={handleSearch}>Search</Button>
+      </CardFooter>
+    </Card>
   )
 }
 
