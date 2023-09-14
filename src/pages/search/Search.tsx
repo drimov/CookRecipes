01fbc444691904afebb65af9d254fcc415a1cf6e
DayOneAI <bambathiam88@gmail.com>
2023-09-14 17:41:46 +0200

    Creation_page_recherche_V2

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -3,6 +3,9 @@ import Card from "./card.tsx"
 import { Input } from "@/components/ui/input"
 
 import React, { useState } from "react"
+import { Search } from "lucide-react"
+
+// import ButtonCateg from "./buttonCateg.tsx"
 
 type SearchPageProps = {
   onSearch: (query: string) => void
@@ -17,27 +20,32 @@ const SearchPage: React.FC<SearchPageProps> = ({ onSearch }) => {
   }
 
   return (
-    <>
-      <header>
+    <div className="place-content-center items-center justify-center p-2">
+      <header className="flex items-center justify-center">
         <div className="m-4 flex flex-col items-center justify-center gap-10 space-x-4 md:flex-row">
           <div className="my-4 flex flex-col justify-center gap-4 p-4">
-            <h3 className="text-5xl font-semibold">Make delicious food</h3>
-            <h3 className=" text-4xl text-orange-500">
+            <h3 className="text-center text-3xl font-semibold md:text-left md:text-5xl">
+              Make delicious food
+            </h3>
+            <h3 className=" text-center text-2xl text-orange-500 md:text-left md:text-4xl">
               and feel experience like a professional chef
             </h3>
-            <p>
+            <p className="text-center text-lg md:text-left">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
               debitis.
             </p>
-            <div className="my-4 flex flex-row space-x-1.5">
+            <div className="my-4 flex flex-row justify-center gap-1 space-x-1.5">
               <Input
-                className="w-80"
+                className="md:w-80"
                 id="name"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 placeholder="Search here ..."
               />
-              <Button onClick={handleSearch}>Search</Button>
+              <Search className="my-auto justify-center md:hidden" />
+              <Button onClick={handleSearch} className="hidden md:block">
+                Search
+              </Button>
             </div>
           </div>
           <div className="">
@@ -50,9 +58,10 @@ const SearchPage: React.FC<SearchPageProps> = ({ onSearch }) => {
         </div>
       </header>
       <div className="m-4 border-t-2"></div>
-      <div className="mb-10 flex flex-col items-center gap-6">
+      <div className="mb-10  flex flex-col items-center gap-6">
         <h3>Highlights</h3>
-        <div className="flex flex-row gap-4">
+        <div className="grid grid-cols-1 items-center gap-4 md:flex md:flex-row">
+          {/* <ButtonCateg /> */}
           <Button>Category 1</Button>
           <Button variant="ghost">Category 2</Button>
           <Button variant="ghost">Category 3</Button>
@@ -60,100 +69,11 @@ const SearchPage: React.FC<SearchPageProps> = ({ onSearch }) => {
           <Button variant="ghost">Category 5</Button>
         </div>
       </div>
-      <div className="mb-16 mt-4 border-t-2"></div>
-      <div className="bor mb-4 grid grid-cols-2 items-center border-2 p-4 md:grid-cols-3 lg:grid-cols-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="mb-16 mt-4 items-center border-t-2"></div>
+      <div className="mb-4 grid grid-cols-1 place-content-center items-center border-2 p-2 sm:grid-cols-2  md:p-6 lg:grid-cols-3 lg:p-6">
         <Card />
       </div>
-
-      {/* <CardContent>
-        <div className="my-4 flex flex-row space-x-1.5">
-          <Input
-            id="name"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search here ..."
-          />
-          <Button onClick={handleSearch}>Search</Button>
-        </div>
-        <div className="flex flex-row justify-evenly space-x-4">
-          <div className="flex flex-col justify-center gap-6 text-4xl font-semibold">
-            <h3>Your favorite food</h3>
-            <h3>Make it good</h3>
-          </div>
-          <img src="../public/hero.png" alt="photo" className="w-96 " />
-        </div>
-        <div className="m-4 border-t-2"></div>
-        <div className=" flex flex-col items-center">
-          <h3>Highlights</h3>
-          <div
-            className="mt-8 flex
-         flex-row gap-4"
-          >
-            <div className="flex flex-col items-center">
-              <img
-                src="../public/gallery-1.jpg"
-                alt="photo"
-                className="w-32 rounded-full"
-              />
-              <h4>Categories 1</h4>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="../public/gallery-1.jpg"
-                alt="photo"
-                className="w-32 rounded-full"
-              />
-              <h4>Categories 1</h4>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="../public/gallery-1.jpg"
-                alt="photo"
-                className="w-32 rounded-full"
-              />
-              <h4>Categories 1</h4>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="../public/gallery-1.jpg"
-                alt="photo"
-                className="w-32 rounded-full"
-              />
-              <h4>Categories 1</h4>
-            </div>
-            <div className="flex flex-col items-center">
-              <img
-                src="../public/gallery-1.jpg"
-                alt="photo"
-                className="w-32 rounded-full"
-              />
-              <h4>Categories 1</h4>
-            </div>
-          </div>
-        </div>
-        <div className="m-4 border-t-2"></div>
-        <div className="relative flex flex-row items-center">
-          <img
-            src="../public/gallery-8.jpg"
-            alt="photo"
-            className="w-96 rounded-md"
-          />
-          <div className=" absolute -left-4 flex w-80 flex-col gap-4 rounded-md bg-orange-500 p-4">
-            <h3>Easy Simple</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-              ab labore voluptates tempora modi facilis praesentium ea.
-              Consequuntur, eveniet quasi.
-            </p>
-            <Button>Read More</Button>
-          </div>
-        </div>
-      </CardContent> */}
-    </>
+    </div>
   )
 }
 
--- src/pages/search/card.tsx
+++ src/pages/search/card.tsx
@@ -1,38 +1,47 @@
 import { AlarmCheck, Star, UtensilsCrossed } from "lucide-react"
+import { dataCategories } from "./searchItems"
+
+// console.log(dataCategories.categories)
 
 const Card = () => {
   return (
     <>
-      <div className="">
-        <div className="relative m-10 flex h-60 w-48 flex-col items-center  space-x-4 rounded-md bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
-          <img
-            src="../public/gallery-6.jpg"
-            alt="photo"
-            className="absolute  -top-10   w-28 rounded-full"
-          />
-          <div className="absolute inset-0 mt-10 flex flex-col items-center justify-center gap-2">
-            <h3 className="text-2xl font-semibold">Plat A</h3>
-            <div className="flex flex-row items-center gap-4">
-              <div className="flex flex-col items-center gap-2">
-                <AlarmCheck />
-                <h3>7 mn</h3>
+      {dataCategories.categories.map((category) => {
+        return (
+          <div key={category.idCategory} className="">
+            <div className="relative m-10 flex h-60 w-48 flex-col items-center   rounded-md bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
+              <img
+                src={category.strCategoryThumb}
+                alt={category.strCategory}
+                className="absolute  -top-10   w-28 rounded-full"
+              />
+              <div className="absolute inset-0 mt-10 flex flex-col items-center justify-center gap-2">
+                <h3 className="text-2xl font-semibold">
+                  {category.strCategory}
+                </h3>
+                <div className="flex flex-row items-center gap-4">
+                  <div className="flex flex-col items-center gap-2">
+                    <AlarmCheck />
+                    <h3>7 mn</h3>
+                  </div>
+                  <div className=" border-r-500"></div>
+                  <div className="flex flex-col items-center gap-2">
+                    <UtensilsCrossed />
+                    <h3>Serving</h3>
+                  </div>
+                </div>
+                <div className="mt-2 flex flex-row items-center gap-2">
+                  <Star />
+                  <Star />
+                  <Star />
+                  <Star />
+                  <Star />
+                </div>
               </div>
-              <div className=" border-r-500"></div>
-              <div className="flex flex-col items-center gap-2">
-                <UtensilsCrossed />
-                <h3>Serving</h3>
-              </div>
-            </div>
-            <div className="mt-2 flex flex-row items-center gap-2">
-              <Star />
-              <Star />
-              <Star />
-              <Star />
-              <Star />
             </div>
           </div>
-        </div>
-      </div>
+        )
+      })}
     </>
   )
 }
