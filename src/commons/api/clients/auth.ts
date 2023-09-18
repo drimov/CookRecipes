e2b2b5a772098108930e736fbf6f2027aaea9ffa
Dimitri Lhopital <dimitrilho@gmail.com>
2023-09-18 15:04:11 +0400

    feat: ✨/ refactor: 🎨retire la complexité inutile et ajout de la fonction pour se connecter

--- src/commons/api/clients/auth.ts
+++ src/commons/api/clients/auth.ts
@@ -1,18 +1,36 @@
 import { supabase } from "@/lib/supabase/client"
 
 async function createUser(email: string, password: string) {
-  const { data, error: errorSignUp } = await supabase.auth.signUp({
+  const { data, error } = await supabase.auth.signUp({
     email,
     password,
   })
+
+  // Check if email is already taken
   const emailIsTaken = data.user?.identities?.length === 0
 
-  const error = {
-    errorAuth: errorSignUp,
-    emailIsTaken,
+  if (error) {
+    throw error
   }
 
-  return { data, error }
+  if (emailIsTaken) {
+    const error = Error("Please use another email")
+    error.name = "Email is already taken"
+    throw error
+  }
+
+  return data
 }
 
-export { createUser }
+async function loginUser(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
+  if (error) {
+    throw error
+  }
+  return data
+}
+
+export { createUser, loginUser }
