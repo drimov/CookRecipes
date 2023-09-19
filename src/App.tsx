044147764443dc45652be40334e2452b18776df0
Dimitri Lhopital <dimitrilho@gmail.com>
2023-09-19 11:48:11 +0400

    feat: ✨ Ajout d'un composant ProtectedRoute pour gérer l'auth. Mise en place du composant, puis déplacement des routes dans un fichier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,8 @@
 import "./App.css"
 
-import { Authentification, Home, Produit, Search } from "./pages"
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-
 import { AppWrapper } from "./context"
-import Error404 from "./pages/Error404"
-import Favorite from "./pages/profile/Favorite"
-import Profile from "./pages/profile/Profile"
-import ProfileLayout from "./pages/profile"
-import RootLayout from "./pages/RootLayout"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <Error404 />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "search",
-        element: <Search />,
-      },
-      {
-        path: "profile",
-        element: <ProfileLayout />,
-        children: [
-          { index: true, element: <Profile /> },
-          { path: "favorite", element: <Favorite /> },
-        ],
-      },
-      {
-        path: "produit",
-        element: <Produit />,
-      },
-      {
-        path: "login",
-        element: <Authentification type="login" />,
-      },
-      {
-        path: "signup",
-        element: <Authentification type="signup" />,
-      },
-    ],
-  },
-])
+import { RouterProvider } from "react-router-dom"
+import { router } from "./routes/config"
 
 const App = () => {
   return (
