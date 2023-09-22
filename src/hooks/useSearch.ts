7a2402746ad3fe0e5655ee8b58bbc21db8b17d2a
Dimitri Lhopital <dimitrilho@gmail.com>
2023-09-22 21:54:40 +0400

    feat: ✨Création d'un hook perso pour gérer la page recherche

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,54 @@
+import { useReducer } from "react"
+
+export type SearchType = "category" | "name"
+type ActionType = SearchType
+
+type State = {
+  activeSearch: SearchType
+  activeValue: string
+  searchTerm: string
+  category: string
+}
+
+type Action =
+  | { type: "category"; payload: string }
+  | { type: "name"; payload: string }
+
+const initialState: State = {
+  activeSearch: "name",
+  activeValue: "",
+  searchTerm: "",
+  category: "All",
+}
+
+const searchReducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "category":
+      return {
+        ...state,
+        activeSearch: action.type,
+        activeValue: action.payload,
+        category: action.payload,
+      }
+    case "name":
+      return {
+        ...state,
+        activeSearch: action.type,
+        activeValue: action.payload,
+        name: action.payload,
+      }
+  }
+}
+
+export const useSearch = () => {
+  const [state, dispatch] = useReducer<React.Reducer<State, Action>>(
+    searchReducer,
+    initialState
+  )
+  const setSearch = (search: string, type: ActionType) => {
+    dispatch({ type, payload: search })
+  }
+
+  const { activeSearch, searchTerm, category, activeValue } = state
+  return { activeSearch, searchTerm, category, activeValue, setSearch }
+}
