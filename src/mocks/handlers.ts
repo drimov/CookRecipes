f68f4db16ac34a3a88003c74f5a2a796c9cb6400
Dimitri Lhopital <dimitrilho@gmail.com>
2023-10-01 10:15:42 +0400

    refactor: 🎨Utilisation optimal de msw

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,7 +3,6 @@ import {
   fakeMealCategories,
   fakeMeals,
   fakeRecipe,
-  fakeUrlImage,
   fakeUserProfile,
 } from "./data"
 
@@ -24,41 +23,79 @@ export const URL_MEAL = {
   RECIPE: `${API_MEAL_ENDPOINTS.RECIPE}.php*`,
 }
 
+const API_ERROR = "api_error"
 const handlersSUPABASE = [
   // GET PROFILE
-  rest.get(URL_DB.PROFILES, async (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(fakeUserProfile))
+  rest.get(URL_DB.PROFILES, async (req, res, ctx) => {
+    const searchTab = req.url.search.split(".")
+    const id = searchTab[searchTab.length - 1]
+    if (id === API_ERROR) {
+      return res(
+        ctx.status(400),
+        ctx.json({ message: "API error when get profile" })
+      )
+    }
+    return res(ctx.status(200), ctx.json({ ...fakeUserProfile, id }))
   }),
   // UPDATE PROFILE
-  rest.post(URL_DB.PROFILES, (_req, res, ctx) => {
+  rest.post(URL_DB.PROFILES, async (req, res, ctx) => {
+    const body = (await req.json()) as unknown
+    if (
+      body &&
+      typeof body === "object" &&
+      "username" in body &&
+      body.username === API_ERROR
+    ) {
+      return res(
+        ctx.status(400),
+        ctx.json({ message: "API error when update profile" })
+      )
+    }
+
     return res(ctx.status(200))
   }),
   // DOWNLOAD AVATAR
-  rest.get(URL_DB.STORAGES, (_req, res, ctx) => {
+  rest.get(URL_DB.STORAGES, async (req, res, ctx) => {
+    const pathTab = req.url.pathname.split("/")
+    const path = pathTab[pathTab.length - 1]
+    if (path === API_ERROR) {
+      return res(
+        ctx.status(400),
+        ctx.json({ message: "API error when download image" })
+      )
+    }
     const newBlob = new Blob([""], { type: "text/html" })
     return res(ctx.status(200), ctx.json(newBlob))
   }),
   // UPLOAD AVATAR
-  rest.post(URL_DB.STORAGES, (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ path: fakeUrlImage }))
+  rest.post(URL_DB.STORAGES, async (req, res, ctx) => {
+    const pathTab = req.url.pathname.split("/")
+    const path = pathTab[pathTab.length - 1]
+    if (path === API_ERROR) {
+      return res(
+        ctx.status(400),
+        ctx.json({ message: "API error when upload image" })
+      )
+    }
+    return res(ctx.status(200), ctx.json({ path }))
   }),
 ]
 
 const handlersAPI = [
   // GET CATEGORIES
-  rest.get(URL_MEAL.CATEGORIES, (_req, res, ctx) => {
+  rest.get(URL_MEAL.CATEGORIES, async (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(fakeMealCategories))
   }),
   // GET MEAL BY NAME
-  rest.get(URL_MEAL.NAME, (_req, res, ctx) => {
+  rest.get(URL_MEAL.NAME, async (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(fakeMeals))
   }),
   // GET MEAL BY CATEGORY
-  rest.get(URL_MEAL.CATEGORY, (_req, res, ctx) => {
+  rest.get(URL_MEAL.CATEGORY, async (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(fakeMealByCategory))
   }),
   // GET RECIPE
-  rest.get(URL_MEAL.RECIPE, (_req, res, ctx) => {
+  rest.get(URL_MEAL.RECIPE, async (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(fakeRecipe))
   }),
 ]
