d3e90425d269dbdc149ccbdf50d65309d711fb7c
Dimitri Lhopital <dimitrilho@gmail.com>
2023-10-01 10:14:05 +0400

    refactor: 🎨utlisation de fetch mock de vitest

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,11 +1,15 @@
 import * as matchers from "@testing-library/jest-dom/matchers"
 
-import { expect } from "vitest"
-import { fetch } from "cross-fetch"
+import { expect, vi } from "vitest"
+
+import { cleanup } from "@testing-library/react"
+import createFetchMock from "vitest-fetch-mock"
 import { server } from "@/mocks/data/server"
 
-global.fetch = fetch
+const fetchMocker = createFetchMock(vi)
+fetchMocker.disableMocks()
 
+beforeEach(() => cleanup())
 beforeAll(() => server.listen({ onUnhandledRequest: `error` }))
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
