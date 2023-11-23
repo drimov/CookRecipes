7dee10c6862ef98142c36baffae93242cedce8bd
Dimitri Lhopital <dimitrilho@gmail.com>
2023-11-23 18:13:06 +0400

    fix: 🐛 correction for new tests path

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default mergeConfig(
     test: {
       globals: true,
       environment: "jsdom",
-      setupFiles: ["./src/tests/setup.ts"],
+      setupFiles: ["./src/__tests__/setup.ts"],
     },
   })
 )
